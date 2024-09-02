da63d104f5de6d6909b2a9e516a5c3909747fc7f
carazhang93 <133140681+carazhang93@users.noreply.github.com>
2024-09-01 22:26:20 -0400

    Task2 Completed

--- extract_data.py
+++ extract_data.py
@@ -13,3 +13,49 @@ def get_stock_history(stock:str)->pd.DataFrame:
     stock_info = yf.Ticker(stock)
     hist = stock_info.history(period='1mo')
     return hist
+
+def get_stock_financials(stock:str) -> pd.DataFrame:
+    """_summary_
+
+    Args:
+        stock (str): _description_
+
+    Returns:
+        pd.DataFrame: _description_
+    """
+    stock_info = yf.Ticker(stock)
+    return stock_info.major_holders
+
+def get_exchange_rate(from_currency:str, to_currency:str, interval:str) -> pd.DataFrame:
+    """_summary_
+
+    Args:
+        from_currency (str): _description_
+        to_currency (str): _description_
+        interval (str): _description_
+
+    Returns:
+        pd.DataFrame: _description_
+    """
+    ticker = f'{from_currency}{to_currency}=X'
+    
+    # Download the historical FX data
+    fx_data = yf.download(ticker, interval=interval)
+    
+    return fx_data
+
+def get_stock_currency_code(stock:str)-> pd.DataFrame:
+    ticker=yf.Ticker(stock)
+    currency_code=ticker.fast_info["currency"]
+    return currency_code
+
+def get_news(stock:str)->pd.DataFrame:
+    stock_info = yf.Ticker(stock)
+    
+    news = stock_info.news
+    
+    news_df = pd.DataFrame(news)
+    
+    return news_df
+
+
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from extract_data import get_stock_history 
+from extract_data import get_stock_history, get_stock_financials, get_exchange_rate, get_stock_currency_code, get_news 
 
 def main():
-    google = get_stock_history('goog')
-    print(google)
-
+    google_hist = get_stock_history('goog')
+    google_major_holder = get_stock_financials('goog')
+    ex_rate = get_exchange_rate("usd","eur","1d") 
+    google_currency_code=get_stock_currency_code('goog')
+    google_news = get_news('goog') 
 
 
 if __name__ =="__main__":
