7c6a5bea2c80c43e4fbaea980648b215fb6dffd5
carazhang93 <133140681+carazhang93@users.noreply.github.com>
2024-09-15 22:12:32 -0400

    add dot and doc string to main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 """main python file for project entry point"""
-from extract_data import (
+from .extract_data import (
     get_stock_history,
     get_stock_financials,
     get_exchange_rate,
@@ -9,11 +9,19 @@ from extract_data import (
 
 
 def main():
+    """
+        main function to retrieve stcok information and history
+    """
     google_hist = get_stock_history("goog")
     google_major_holder = get_stock_financials("goog")
     ex_rate = get_exchange_rate("usd", "eur", "1d")
     google_currency_code = get_stock_currency_code("goog")
     google_news = get_news("goog")
+    print(google_hist)
+    print(google_major_holder)
+    print(ex_rate)
+    print(google_currency_code)
+    print(google_news)
 
 
 if __name__ == "__main__":
