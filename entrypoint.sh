42f18d21206c7fad39964b67290d7b6586171faa
carazhang93 <133140681+carazhang93@users.noreply.github.com>
2025-06-20 18:07:36 -0400

    updated entrypoint.sh by make echo lowercase and add the sleep command

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,9 @@
 airflow db init
 airflow users create --username admin --password admin --firstname kobe --lastname Gao --role Admin --email hgao62@uwo.ca
 airflow webserver -p 8080
-ECHO "Airflow webserver is running"
-ECHO "About to run Airflow scheduler"
+echo "Airflow webserver is running"
+
+sleep 10
+echo "About to run Airflow scheduler"
 airflow scheduler
-ECHO "Airflow scheduler is running"
+echo "Airflow scheduler is running"
