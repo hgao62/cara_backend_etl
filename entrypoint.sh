e5785ff5ac11675c735cbd2d9a5b6e74661340ff
carazhang93 <133140681+carazhang93@users.noreply.github.com>
2025-06-21 22:19:16 -0400

    revert entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 airflow db init
 airflow users create --username admin --password admin --firstname kobe --lastname Gao --role Admin --email hgao62@uwo.ca
-airflow webserver -p 8080
+airflow webserver -p 8080 &
 echo "Airflow webserver is running"
 
-# sleep 10
-# echo "About to run Airflow scheduler"
-# airflow scheduler
-# echo "Airflow scheduler is running"
+sleep 10
+echo "About to run Airflow scheduler"
+airflow scheduler
+echo "Airflow scheduler is running"
